f9d46c036554360a43a38094d01c2593d0d7dcb4
yiztak <yonosoyyiztak@hotmail.com>
2025-03-17 19:17:42 -0600

    background and sounds

--- app/src/main/java/com/example/projectgun/MainActivity.kt
+++ app/src/main/java/com/example/projectgun/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
 import android.content.Context
+import android.hardware.camera2.CameraManager
 import android.media.MediaPlayer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,6 +20,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity(), SensorEventListener {
     var backgroundMediaPlayer: MediaPlayer? = null
@@ -57,19 +62,22 @@ class MainActivity : ComponentActivity(), SensorEventListener {
             _sensorValues.value = Triple(x, y, z)
             if((x>-10||x<10)&&(y>-10||y<10)&&(z>-10||z<10)){
                 if ((x < 2.0 && x > -2.0) && (y < -9.5) && (z < 4.0 && z > -4.0)&&fase==1) {
-                    vibrarCelular(500)
-
-                    fase=0
                     reproducirSonido(this, R.raw.revolver_prepare)
-                }else if((x < -2.0) && (y > -9.5) && (z < 4.0 && z > -4.0)&&fase!=1&&fase!=2){
+                    fase=0
                     vibrarCelular(500)
-                    fase=1
+                }else if((x < -2.0) && (y > -9.5) && (z < 4.0 && z > -4.0)&&fase!=1&&fase!=2){
                     reproducirSonido(this,R.raw.spining)
+                    fase=1
+                    vibrarCelular(500)
                 }else if(fase==1 &&(y-yAnterior>2&&(tiempoActual-tiempoUltimoDisparo>500))&&(y>2)&&fase==1){
+                    controlFlash(true)
+                    CoroutineScope(Dispatchers.Main).launch{
+                        delay(300)
+                        controlFlash(false)
+                    }
+                    reproducirSonido(this, R.raw.revolver_shoot)
                     vibrarCelular(100)
                     tiempoUltimoDisparo= tiempoActual
-                    reproducirSonido(this, R.raw.revolver_shoot)
-
                     fase=2
                 }else if (fase == 2 && y > -9.5) {
                     fase = 1
@@ -125,7 +133,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     }
 
 
-    private fun vibrarCelular(milisegundos: Long) {
+    fun vibrarCelular(milisegundos: Long) {
         val vibrator = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
             val manager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
             manager.defaultVibrator
@@ -138,6 +146,12 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         } else {
             vibrator.vibrate(milisegundos)
         }
+    }
+
+    fun controlFlash(state:Boolean){
+        val cameraManager=getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        cameraManager.setTorchMode(cameraId,state)
     }
 }
 
