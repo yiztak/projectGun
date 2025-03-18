88f4ff641c09557e48be97490f9f01dd150f048b
yiztak <yonosoyyiztak@hotmail.com>
2025-03-17 18:10:15 -0600

    gun sound

--- app/src/main/java/com/example/projectgun/MainActivity.kt
+++ app/src/main/java/com/example/projectgun/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     var accelerometer: Sensor? = null
     var _sensorValues = mutableStateOf(Triple(0f, 0f, 0f))
     var fase=0
+    var yAnterior=0f
+    var tiempoUltimoDisparo=System.currentTimeMillis()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,24 +47,44 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
     override fun onSensorChanged(event: SensorEvent?) {
         event?.let {
-
+            val tiempoActual=System.currentTimeMillis()
             val x = it.values[0]
             val y = it.values[1]
             val z = it.values[2]
             _sensorValues.value = Triple(x, y, z)
+            if((x>-10||x<10)&&(y>-10||y<10)&&(z>-10||z<10)){
+                if ((x < 2.0 && x > -2.0) && (y < -9.5) && (z < 4.0 && z > -4.0)&&fase==1) {
+                    vibrarCelular(500)
 
-            if ((x < 4.0 || x > -4.0) && (y < -7.0) && (z < 4.0 || z > -4.0)&&fase!=0) {
-                vibrarCelular(500)
+                    fase=0
+                }else if((x < -2.0) && (y > -9.5) && (z < 4.0 && z > -4.0)&&fase!=1&&fase!=2){
+                    vibrarCelular(500)
+                    fase=1
 
-                fase=0
-            }else if((x < -4.0) && (y > -9.0) && (z < 4.0 || z > -4.0)&&fase==0){
-                vibrarCelular(500)
-                
-                fase=1
+                }else if(fase==1 &&(y-yAnterior>1&&(tiempoActual-tiempoUltimoDisparo>500))&&(y>2)&&fase==1){
+                    vibrarCelular(100)
+                    tiempoUltimoDisparo= tiempoActual
+                    reproducirSonido(this, R.raw.revolver_shoot)
+
+                    fase=2
+                }else if (fase == 2 && y > -9.5) {
+                    fase = 1
+                }
+                yAnterior=y
+
             }
-
         }
+
     }
+
+    private var mediaPlayer: MediaPlayer? = null
+
+    fun reproducirSonido(context: Context, sonidoResId: Int) {
+        mediaPlayer?.release()
+        mediaPlayer = MediaPlayer.create(context, sonidoResId)
+        mediaPlayer?.start()
+    }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         // No es obligatorio manejar esto
